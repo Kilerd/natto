6b42e94c559b9690d98473f9bb03473293277a93
Kilerd Chan <blove694@gmail.com>
2024-09-20 23:51:59 +0800

    feat: add warning for no pk table

--- frontend/src/routes/tableList.tsx
+++ frontend/src/routes/tableList.tsx
@@ -4,6 +4,8 @@ import { useEffect, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { columnDefsGenrator } from '@/components/base/tableUtils';
 import { DataTable } from '@/components/base/DataTable';
+import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
 
 
 
@@ -28,10 +30,23 @@ const TableList: React.FC = () => {
 
     return (
         <>
+
+
             <div className="container mx-auto px-4 ">
                 <div className="flex justify-between items-center py-10">
                     <h1 className="text-2xl font-bold">Table {tableName}</h1>
                 </div>
+
+                {tables[tableName]?.has_pk_key === false && (
+
+                    <Alert className="bg-yellow-100 border-yellow-400 text-yellow-700">
+                        <ExclamationTriangleIcon className="h-4 w-4 text-yellow-400" />
+                        <AlertTitle>Warning</AlertTitle>
+                        <AlertDescription>
+                            Delete and edit functionality is disabled for this table as it does not have a primary key.
+                        </AlertDescription>
+                    </Alert>
+                )}
                 <div className="container mx-auto">
                     <div >
                         <DataTable tableName={tableName} columns={columns} data={tableData.state === 'hasData' ? tableData.data : []} columnDefinitions={tables[tableName]?.columns ?? []} />
